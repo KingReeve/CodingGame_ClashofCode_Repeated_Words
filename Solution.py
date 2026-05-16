2cc245d8dbb9c46d5e96c021d8b3d18895d04806
KingReeve <140770868+KingReeve@users.noreply.github.com>
2026-05-16 18:00:30 -0500

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -1,32 +1,39 @@
-string = input()
-array = string.replace('\\n',' ').split()
-result = []
-for l,r in zip(array,array[1:]):
-    if not l[-1].isalpha() or not r[0].isalpha():
+def clean(s) -> str:
+    if not s[0].isalpha():
+        for i in range(len(s)):
+            if s[i].isalpha():
+                return s[i:]
+    else:
+        for i in range(len(s)-1,-1,-1):
+            if s[i].isalpha():
+                return s[:i+1]
+    return ""
+
+array = input().replace('\\n',' ').split()
+
+index = 0
+words = 0
+
+while index<len(array)-1:
+    if array[index][-1].isalpha():
+        test = clean(array[index]).lower()
+    else:
+        index+=1
         continue
-    if not l[0].isalpha(): 
-        hold=[]
-        seen=False
-        for i in l:
-            if i.isalpha():
-                seen=True
-            if seen:
-                hold.append(i)
-        l_clean=''.join(hold)
-    else:
-        l_clean=l
-    if not r[-1].isalpha():
-        idx=-1
-        for i in range(len(r)-1,-1,-1):
-            if r[i].isalpha():
-                idx=i
-                break
-        r_clean=r[:idx+1]
-    else:
-        r_clean=r
-    if l_clean.lower()==r_clean.lower():
-        result.append(l_clean)
-if result:
-    for i in result:
-        print(i)
-else: print('No repeats!')
+    count=0
+    for i in range(index+1,len(array)):
+        if not array[i][0].isalpha():
+            break
+
+        if array[i].lower()!=test:
+            if clean(array[i]).lower()==test:
+                count+=1
+            break
+        count+=1
+    if count>0:
+        print(clean(array[index]))
+        words+=1
+        index+=count
+    index+=1
+if words==0:
+    print('No repeats!')
